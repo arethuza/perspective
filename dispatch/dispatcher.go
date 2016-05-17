2688cc00f7a54e973ea3ec94947f4479e81ebe7c
Alan F. Slater <alan.f.slater@gmail.com>
2016-05-17 23:18:26 +0100

    Obtain path, action and args from request and pass to dispatcher

--- dispatch/dispatcher.go
+++ dispatch/dispatcher.go
@@ -7,3 +7,16 @@ type dispatcher struct {
 func Dispatcher() *dispatcher {
 	return &dispatcher{}
 }
+
+func (d *dispatcher) Process(path, method, action string, args *map[string]string) (interface{}, *HttpError) {
+	return nil, &HttpError{}
+}
+
+type HttpError struct {
+	Code int
+	message string
+}
+
+func (he HttpError) Error() string {
+	return he.message
+}
--- server.go
+++ server.go
@@ -1,17 +1,31 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
 	"path"
 	dispatch "github.com/arethuza/perspective/dispatch"
+	"strings"
 )
 
 var dispatcher = dispatch.Dispatcher()
 
 func handler(w http.ResponseWriter, r *http.Request) {
 	path := path.Clean(r.URL.Path)
-	fmt.Fprintf(w, "%s\n", path)
+	method := strings.ToLower(r.Method)
+	r.ParseForm()
+	action := r.Form.Get("action")
+	args := make(map[string]string)
+	for name, value := range r.Form {
+		args[strings.ToLower(name)] = value[0]
+	}
+	delete(args, "action")
+	result, err := dispatcher.Process(path, method, action, &args)
+	if err != nil {
+		http.Error(w, err.Error(), err.Code)
+	} else {
+		switch result.(type) {
+		}
+	}
 }
 
 func main() {
