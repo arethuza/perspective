8db03a2dadb54c418faa0814109f28e1307d9f8e
Alan F. Slater <alan.f.slater@gmail.com>
2016-10-12 23:05:54 +0100

    Started manager database creation script

---
+++ database/manager.sql
@@ -0,0 +1,13 @@
+﻿drop table if exists tenancy;
+
+create table tenancy
+(
+    id 			        serial      primary key,
+    name		        text        unique not null,
+    database_name	    text        unique not null,
+    admin_user		    text        not null,
+    admin_password_hash	text        not null,
+    status		        smallint    not null default 0,
+    created_create      timestamp   default current_date
+)
+
